eae2e1f9bf07284b683e593e144a0aaaf9d429aa
Joseph Bagaric <joseph@josephit.com>
2019-05-21 10:02:11 +0200

    Compile for byzantium and optimize

--- truffle-config.js
+++ truffle-config.js
@@ -18,9 +18,12 @@ module.exports = {
   compilers: {
     solc: {
       version: "0.5.2",
-      optimizer: {
-        enabled: true,
-        runs: 200
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        },
+        evmVersion: "byzantium"
       }
     }
   }
